2b3e8aba0367016b7fef3a2f7c9e191431d91793
noureldin <noureldin2662002@gmail.com>
2026-01-29 18:56:54 +0200

    init

---
+++ src/main/java/com/example/vet_mvc_app/VetMvcAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.vet_mvc_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetMvcAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VetMvcAppApplication.class, args);
+    }
+
+}
